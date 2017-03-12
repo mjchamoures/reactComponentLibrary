4f78d7a437f9195f108b062c3c70d1c0e67b65c2
mchamour <mchamour@cisco.com>
2017-03-11 18:08:53 -0800

    Add moves list

--- src/client/app/TicTacToeGameComponent.jsx
+++ src/client/app/TicTacToeGameComponent.jsx
@@ -79,6 +79,10 @@ class TicTacToeGameComponent extends React.Component {
     }
   }
 
+  jumpTo(move) {
+
+  }
+
   render() {
     const history = this.state.history;
     const current = history[history.length-1];
@@ -91,6 +95,18 @@ class TicTacToeGameComponent extends React.Component {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+        'Move #' + move :
+        'Game Start';
+
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
 
     return (
       <div className="game">
@@ -102,7 +118,7 @@ class TicTacToeGameComponent extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );

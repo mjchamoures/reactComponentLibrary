ed2d8a5694043984e551d11b0ea3b5bdd4b0b0b9
mchamour <mchamour@cisco.com>
2017-03-11 18:56:42 -0800

    Add jumpTo functionality to ticTacToe

--- src/client/app/TicTacToeGameComponent.jsx
+++ src/client/app/TicTacToeGameComponent.jsx
@@ -45,26 +45,24 @@ class Board extends React.Component {
 class TicTacToeGameComponent extends React.Component {
 
   constructor() {
-
     super();
-
+    
     this.state = {
-
       history : [{
         squares : Array(9).fill(null),
       }],
       xIsNext : true,
-
+      stepNumber : 0,
     };
-
   }
-
+  
   handleClick(i) {
+    // var history = this.state.history.slice(0, this.state.stepNumber + 1);
     const history = this.state.history;
-    const current = history[history.length-1];
+    const current = history[history.length - 1];
     // We call .slice() to copy the squares array instead of mutating the existing array
     const squares = current.squares.slice();
-
+    
     // return early and ignore the click if someone has already won the game or if a square is already filled:
     if(calculateWinner(squares) || squares[i]) {
       return;
@@ -72,20 +70,26 @@ class TicTacToeGameComponent extends React.Component {
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         history : history.concat([{
-          squares : squares,
+          squares : squares
         }]),
         xIsNext : !this.state.xIsNext,
+        stepNumber :  history.length
       });
     }
   }
-
-  jumpTo(move) {
-
+  
+  jumpTo(step) {
+    // set xIsNext to true if the index of the move number is an even number.
+    this.setState({
+      stepNumber : step,
+      xIsNext : (step % 2) ? false : true,
+    });
+    
   }
-
+  
   render() {
     const history = this.state.history;
-    const current = history[history.length-1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     
     let status;
@@ -94,26 +98,26 @@ class TicTacToeGameComponent extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-
+    
+    // step is val, move is index
     const moves = history.map((step, move) => {
+      
       const desc = move ? 
         'Move #' + move :
-        'Game Start';
-
+        'Game start';
       return (
         <li key={move}>
           <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       );
     });
-
-
+    
     return (
       <div className="game">
         <div className="game-board">
           <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            squares={current.squares} 
+            onClick={(i) => this.handleClick(i) }
           />
         </div>
         <div className="game-info">
